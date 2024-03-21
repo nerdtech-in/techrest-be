9b7fe83da049ab38b9d070003ff48129661fad1e
NERDTECH(R) <159178280+nerdtech-in@users.noreply.github.com>
2024-03-21 16:51:40 +0530

    Update settings.py

--- techrest/settings.py
+++ techrest/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt',
     "corsheaders",
     'mathfilters',
+    'whitenoise.runserver_nostatic',
 ]
 
 MIDDLEWARE = [
@@ -56,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'techrest.urls'
@@ -125,7 +127,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / "media"
 
@@ -136,7 +141,7 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-DOMAIN_NAME = "http://192.168.29.33:3000/#"
+DOMAIN_NAME = "https://app.techrest.in/#"
 
 CHANNEL_LAYERS = {
     'default': {
@@ -166,4 +171,5 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:9000",
     "http://192.168.29.33:3000",
+    "https://app.techrest.in"
 ]
